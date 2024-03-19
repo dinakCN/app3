b0a7657fc5ad381ce173acfda2b5fb7f24a1b08b
dNc <dinak@cn.ru>
2024-03-19 22:16:13 +0700

    123

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -10,17 +10,28 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import 'vuetify/styles'
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 import { aliases, fa } from 'vuetify/iconsets/fa'
 import { mdi } from 'vuetify/iconsets/mdi'
 import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
 import { useI18n } from 'vue-i18n'
 import i18n from './vue-i18n.ts'
 
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#F2F5F8',
+    surface: '#FFFFFF',
+  },
+}
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: 'myCustomLightTheme',
+    themes: {
+      myCustomLightTheme,
+    },
   },
   icons: {
     defaultSet: 'fa',
